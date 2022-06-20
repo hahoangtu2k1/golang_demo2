0d4e14b39bef4c143d7170997e7932eb106e7f7f
hahoangtu2k1 <76681126+hahoangtu2k1@users.noreply.github.com>
2022-06-20 11:24:30 +0700

    add fix

--- main.go
+++ main.go
@@ -27,10 +27,9 @@ func main() {
 func test1() {
 	fmt.Println("----------------------------------bài 1-----------------------------------------")
 	for i := 0; i < 3; i++ {
-		time.Sleep(3 * time.Second)
 		fmt.Println("time now: ", time.Now().UnixMilli())
+		time.Sleep(3 * time.Second)
 	}
-	time.Sleep(3 * time.Second)
 	fmt.Println("kết thúc")
 
 	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
@@ -47,27 +46,25 @@ func test1() {
 
 func test2() {
 	fmt.Println("----------------------------bài 2-------------------------------")
-	t := time.Now().UnixMilli()
+	t := time.Now().Unix()
 	// formatted := fmt.Sprintf("date time: ", t.Day())
 	fmt.Println("date time", t)
 }
+
 func test3() {
 	fmt.Println("----------------------------bài 3-------------------------------")
 	ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
-	go func(ctx context.Context) {
-		for {
-			time.Sleep(time.Second * 3)
-			select {
-			case <-ctx.Done():
-				fmt.Println("Cancelled:", ctx.Err())
-				return
-			default:
-				fmt.Println("Sleep")
-			}
+	for i := 1; i <= 3; i++ {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Cancelled:", ctx.Err())
+			return
+		case <-time.After(3 * time.Second):
+			fmt.Println("Sleep lan thu: ", i)
 		}
-	}(ctx)
-	time.Sleep(10 * time.Second)
+	}
 }
+
 func test4() {
 	fmt.Println("----------------------------bài 4-------------------------------")
 
@@ -79,11 +76,13 @@ func test5() {
 	d := time.Unix(0, 592190385).UTC()
 	fmt.Println("day: ", d.Weekday())
 }
+
 func test6() {
 	fmt.Println("----------------------------bài 6-------------------------------")
 
 	fmt.Println("Có 4 loại :seconds, milliseconds, microseconds and nanoseconds")
 }
+
 func test7() {
 	fmt.Println("----------------------------bài 7-------------------------------")
 
@@ -101,26 +100,30 @@ func test7() {
 	fmt.Println(result)
 
 }
+
 func test8() {
 	fmt.Println("----------------------------bài 8-------------------------------")
-
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
 	for {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Println(time.Now().Unix(), "done")
+		select {
+		case <-ctx.Done():
+			fmt.Println("Cancelled:", ctx.Err())
+			return
+		case <-time.After(100 * time.Millisecond):
+			fmt.Println(time.Now().Unix(), "done")
+		}
 	}
 }
 
 func test9() {
 	fmt.Println("----------------------------bài 9------------------------------")
-
-	step := time.Duration(100 * time.Millisecond)
 	f := func() {
 		fmt.Println("I'm study")
 	}
-
-	Timer1 := time.AfterFunc(step, f)
+	Timer1 := time.AfterFunc(time.Duration(100*time.Millisecond), f)
 	defer Timer1.Stop()
-	time.Sleep(10 * time.Second)
+	// Calling sleep method
+	time.Sleep(2 * time.Second)
 	/*time.AfterFunc(time.Millisecond*100, func() {
 		fmt.Println("i'm study")
 	})
